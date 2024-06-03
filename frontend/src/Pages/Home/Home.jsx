5d2ad72ec25e05be268b9f85ada1c47e3171703e
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-06-04 01:22:15 +0530

    completed explore menu ui

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 // import React from 'react'
+import ExpMenu from '../../components/ExploreMenu/ExpMenu'
 import Header from '../../components/Header/Header'
 import './Home.css'
 const Home = () => {
   return (
     <div>
       <Header />
+      <ExpMenu />
     </div>
   )
 }
