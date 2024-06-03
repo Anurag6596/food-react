65838250ef38327d68fb7540b9e22222ceed40d5
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-06-04 02:51:17 +0530

    given functionality to the explore menu using useState useEffect and props

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 // import React from 'react'
+import { useState } from 'react'
 import ExpMenu from '../../components/ExploreMenu/ExpMenu'
 import Header from '../../components/Header/Header'
 import './Home.css'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 const Home = () => {
+
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
       <Header />
-      <ExpMenu />
+      <ExpMenu category={category} setCategory={setCategory}/>  {/*initailizing props*/}
+      <FoodDisplay category={category}/>
     </div>
   )
 }
--- frontend/src/components/ExploreMenu/ExpMenu.jsx
+++ frontend/src/components/ExploreMenu/ExpMenu.jsx
@@ -2,7 +2,7 @@
 import './ExpMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExpMenu = () => {
+const ExpMenu = ({category,setCategory}) => {
   return (
     <div className='exp-menu' id='menu'>
         <h1>Explore our Menu</h1>
@@ -10,8 +10,8 @@ const ExpMenu = () => {
         <div className="explore-menu-list">
             {menu_list.map((item,index) => {
                 return (
-                    <div key={index} className="explore-menu-list-item">
-                        <img src={item.menu_image} alt="" />
+                    <div onClick={()=>setCategory(prev=>prev=== item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
