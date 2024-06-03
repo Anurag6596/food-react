de11ff8573d7380a6426c52c99c125cdc00c8fe0
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-06-03 22:51:26 +0530

    header component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 // import React from 'react'
 
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
-
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cards/Cart'
+import PlaceOrd from "./Pages/PlaceOrder/PlaceOrd"
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/PlaceOrder" element={<PlaceOrd />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food</h2>
+            <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Pune, based on trends</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
