78a43bce41b217fbd008a0f5ecfcd839e79e17b8
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-06-22 22:04:04 +0530

    done some of the navigation changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets.js";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 // import { Link } from 'react-router-dom'
 const Navbar = () => {
 
@@ -10,10 +11,10 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <sli className={menu==="home"?"active":""} onClick={()=> setMenu("home")}>Home</sli>
-        <li className={menu==="Menu"?"active":""} onClick={()=> setMenu("Menu")}>Menu</li>
-        <li className={menu==="Mobile App"?"active":""} onClick={()=> setMenu("Mobile App")}>Mobile App</li>
-        <li className={menu==="Contact Us"?"active":""} onClick={()=> setMenu("Contact Us")}>Contact Us</li>
+        <Link to='/' className={menu==="home"?"active":""} onClick={()=> setMenu("home")}>Home</Link>
+        <a href="#menu" className={menu==="Menu"?"active":""} onClick={()=> setMenu("Menu")}>Menu</a>
+        <a href="#app-download" className={menu==="Mobile App"?"active":""} onClick={()=> setMenu("Mobile App")}>Mobile App</a>
+        <a href="#footer" className={menu==="Contact Us"?"active":""} onClick={()=> setMenu("Contact Us")}>Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
