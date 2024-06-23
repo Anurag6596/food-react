7c97136217d3d0514dd58e6c34f95607c656e6c2
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-06-23 22:31:45 +0530

    minor changes done in add to cart option

--- frontend/src/Pages/Cards/Cart.jsx
+++ frontend/src/Pages/Cards/Cart.jsx
@@ -2,7 +2,7 @@
 
 import { useContext } from "react"
 import { StoreContext } from "../../Context/storeContext"
-
+import './Cart.css'
 const Cart = () => {
 
      const {cartItems,food_list,removeFromCart} = useContext(StoreContext)
@@ -20,7 +20,21 @@ const Cart = () => {
           </div>
           <br />
           <hr />
-          
+          {food_list.map((item,index)=>{
+              if(cartItems[item._id]>0)
+                {
+                  return(
+                    <div key={index} className="cart-items-title cart items item">
+                        <img src={item.image} alt="" />
+                        <p>{item.name}</p>
+                        <p>${item.price}</p>
+                        <p>{cartItems[item._id]}</p>
+                        <p>${item.price*cartItems[item._id]}</p>
+                        <p>x</p>
+                    </div>
+                  )
+                }
+          })}
       </div>
     </div>
   )
