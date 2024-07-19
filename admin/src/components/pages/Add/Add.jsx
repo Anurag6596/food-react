23bd7345e82d71dc794ccf60a0d3debf525498bb
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-07-20 02:34:02 +0530

    completed add page ui

--- admin/src/components/pages/Add/Add.jsx
+++ admin/src/components/pages/Add/Add.jsx
@@ -1,12 +1,50 @@
 // import React from 'react'
-import './Add.css';
-
+import { assets } from '../../../assets/assets';
+import './Add.css'
 
 
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='add'>
+        <form className='flex-col'>
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor="image">
+                    <img src={assets.upload_area} alt="" />
+                    <input type="file" id='image' hidden required/>
+                </label>
+            </div>
+            <div className="add-product-name flex-col">
+                <p>Product Name</p>
+                <input type="text" name='name' placeholder='type here'/>
+            </div>
+            <div className="add-product-dicription flex-col">
+                <p>Prduct Discription</p>
+                <textarea name="discription"  rows='6' placeholder='Write your description here' required> </textarea>
+            </div>
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product Category</p>
+                    <select name="category">
+                        <option value="Salad">Salad</option>
+                        <option value="Rolls">Rolls</option>
+                        <option value="Desert">Desert</option>
+                        <option value="Sandwich">Sandwich</option>
+                        <option value="Cake">Cake</option>
+                        <option value="Pure Veg">Pure Veg</option>
+                        <option value="Pasta">Pasta</option>
+                        <option value="Noodles">Noodles</option>
+                    </select>
+                </div>
+                <div className="add-price flex-col">
+                    <p>Product Price</p>
+                    <input type="number" name='price' placeholder='$20' />
+                </div>
+            </div>
+            <button type='submit' className='add-btn'>Add</button>
+        </form>
+    </div>
   )
 }
 
