95fcc522d05fc8915daaa657acf8afcc6573c3db
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2024-07-20 01:58:00 +0530

    added the routes to admin section

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 // import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
+import Add from './components/pages/Add/Add';
+import List from './components/pages/List/List';
+import Orders from './components/pages/Orders/Orders';
 
 
 
@@ -14,6 +18,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes >
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
